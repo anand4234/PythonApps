42887e985f9afa8799466334eab15f8710db289c
Anand Pavithran <anandpavithran9@gmail.com>
2020-05-09 18:44:06 +0530

    Added close matches query.

--- Dictman.py
+++ Dictman.py
@@ -1,4 +1,6 @@
 import json
+import difflib
+from difflib import get_close_matches
 
 data = json.load(open('data.json'))
 
@@ -7,12 +9,21 @@ def sel(word):
     if word in data:
         return data[word]
     else:
-        print("Double check the word!")
+        nw = get_close_matches(word, data.keys(), n=1)
+        print('Did you mean ' + nw[0] + '? y or n?')
+        ri = input()
+        if ri == 'y':
+            return data[nw[0]]
+        elif ri == 'n':
+            print("Try Again!")
+        else:
+            print("Not in dictionary!")
 
 
-word = input("enter word")
-lists = sel(word)
+word = input("Enter Word?\n")
+lists = sel(word.lower())
 num = 1
-for i in lists:
-    print(str(num) + ". " + i)
-    num = num + 1
+if (lists):
+    for i in lists:
+        print(str(num) + ". " + i)
+        num = num + 1
